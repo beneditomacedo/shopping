54b491d70c395836d530f8a96a39a681ac8385ed
Benedito Alberto Macedo <benedito.macedo@gmail.com>
2019-10-20 22:42:45 -0300

    Store, Order, Item models added

--- shopping/models.py
+++ shopping/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class Store(models.Model):
+    address = models.CharField(max_length=255)
+
+class Order(models.Model):
+    date = models.DateField()
+    store = models.ForeignKey(Store,on_delete=models.CASCADE)
+    number = models.IntegerField()
+    serie = models.IntegerField()
+    key = models.CharField(max_length=44)
+    quantity_items = models.IntegerField()
+    value = models.DecimalField(decimal_places=2,max_digits=11)
+    url = models.CharField(max_length=255)
+
+class Item(models.Model):
+    order = models.ForeignKey(Order,on_delete=models.CASCADE)
+    code = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    quantity = models.IntegerField()
+    unit = models.CharField(max_length=255)
+    value = models.DecimalField(decimal_places=2,max_digits=11)
+    total_value = models.DecimalField(decimal_places=2,max_digits=11)
