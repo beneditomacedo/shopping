a66ab98b9d8a97fd52739d31cfec1aa8c6b53737
Benedito Alberto Macedo <benedito.macedo@gmail.com>
2019-11-10 22:40:31 -0300

    admin interface customized to inlines

--- shopping/admin.py
+++ shopping/admin.py
@@ -2,7 +2,21 @@ from django.contrib import admin
 
 from .models import Retail, Store, Order, Item
 
-admin.site.register(Retail)
-admin.site.register(Store)
-admin.site.register(Order)
-admin.site.register(Item)
+class StoreInLine(admin.TabularInline):
+    model = Store
+
+class RetailAdmin(admin.ModelAdmin):
+    inlines = [
+        StoreInLine,
+    ]
+
+class ItemInLine(admin.TabularInline):
+    model = Item
+
+class OrderAdmin(admin.ModelAdmin):
+    inlines = [
+        ItemInLine
+    ]
+
+admin.site.register(Retail,RetailAdmin)
+admin.site.register(Order,OrderAdmin)
