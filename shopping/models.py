5e0a8c2b393e1da4474af6760f563c8e596a912a
Benedito Alberto Macedo <benedito.macedo@gmail.com>
2019-11-25 20:12:49 -0300

    rename Order attribute "url" to "url_order"

--- shopping/models.py
+++ shopping/models.py
@@ -1,13 +1,16 @@
+"""  models """
 from django.db import models
 
 class Retailer(models.Model):
+    """  Retailer """
     name = models.CharField(max_length=255)
 
     def __str__(self):
         return self.name
 
 class Store(models.Model):
-    retail = models.ForeignKey(Retailer,on_delete=models.CASCADE)
+    """  Store """
+    retail = models.ForeignKey(Retailer, on_delete=models.CASCADE)
     cnpj = models.CharField(max_length=14)
     address = models.CharField(max_length=255)
 
@@ -15,26 +18,28 @@ class Store(models.Model):
         return self.cnpj
 
 class Order(models.Model):
+    """  Order """
     date = models.DateField()
-    store = models.ForeignKey(Store,on_delete=models.CASCADE)
+    store = models.ForeignKey(Store, on_delete=models.CASCADE)
     number = models.IntegerField()
     serie = models.IntegerField()
     key = models.CharField(max_length=44)
     quantity_items = models.IntegerField()
-    value = models.DecimalField(decimal_places=2,max_digits=11)
-    url = models.CharField(max_length=255)
+    value = models.DecimalField(decimal_places=2, max_digits=11)
+    url_order = models.CharField(max_length=255)
 
     def __str__(self):
         return self.key
 
 class Item(models.Model):
-    order = models.ForeignKey(Order,on_delete=models.CASCADE)
+    """  Item """
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
     code = models.CharField(max_length=255)
     description = models.CharField(max_length=255)
     quantity = models.IntegerField()
     unit = models.CharField(max_length=255)
-    value = models.DecimalField(decimal_places=2,max_digits=11)
-    total_value = models.DecimalField(decimal_places=2,max_digits=11)
+    value = models.DecimalField(decimal_places=2, max_digits=11)
+    total_value = models.DecimalField(decimal_places=2, max_digits=11)
 
     def __str__(self):
         return self.description
